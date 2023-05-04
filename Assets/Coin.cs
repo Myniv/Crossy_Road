5e475f0749445c06210662ffbbcbf86715a5ad62
mulyanan26 <mulyanan.ur26@gmail.com>
2023-05-05 05:59:45 +0700

    Day 5 Part 1
    Add Score
    Add Coin so  if character get the coin, he will get much score
    Add Eagle

---
+++ Assets/Coin.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] int value = 1;
+    [SerializeField, Range (0,10)] float rotationSpeed = 1;
+
+    public int Value { get => value;}
+
+    //Animasi untuk mengambil Coin
+    public void Collected()
+    {
+        GetComponent<Collider>().enabled=false;
+        rotationSpeed*=2;
+        this.transform.DOJump(this.transform.position,
+                              1,
+                              1,
+                              0.5f).onComplete=SelfDestruct;
+    }
+
+    //Ketika coin telah diambil maka coin tersebut dihapus
+    private void SelfDestruct(){
+        Destroy(this.gameObject);
+    }
+
+    private void Update() {
+        transform.Rotate(0,360*rotationSpeed*Time.deltaTime,0);
+    }
+}
