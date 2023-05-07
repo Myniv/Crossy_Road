a382183db323de77343611e045113efe6662df1b
mulyanan26 <mulyanan.ur26@gmail.com>
2023-05-07 16:42:01 +0700

    Using different asset

--- Assets/Script/Duck.cs
+++ Assets/Script/Duck.cs
@@ -6,24 +6,28 @@ using UnityEngine.Events;
 
 public class Duck : MonoBehaviour
 {
-    //pp
-    [SerializeField, Range(0,1)] float moveDuration = 0.1f;
-    [SerializeField, Range(0,1)] float jumpHeight = 0.5f;
+
+    [SerializeField, Range(0, 1)] float moveDuration = 0.1f;
+    [SerializeField, Range(0, 1)] float jumpHeight = 0.5f;
 
     [SerializeField] int leftMoveLimit;
     [SerializeField] int rightMoveLimit;
     [SerializeField] int backMoveLimit;
-  
-    public UnityEvent OnParticleStart;
+
+    public UnityEvent OnStart;
     public UnityEvent<Vector3> OnJumpEnd;
     public UnityEvent<int> OnGetCoin;
     public UnityEvent OnCarCollision;
     public UnityEvent OnDie;
 
+    private Animator animatorSet;
+
     bool isNotMoveable = false;
-    
-    private void Start() {
-        OnParticleStart.Invoke();
+
+    private void Start()
+    {
+        OnStart.Invoke();
+        animatorSet = GetComponent<Animator>();
     }
     void Update()
     {
@@ -36,28 +40,45 @@ public class Duck : MonoBehaviour
             return;
         }
 
+
+
         Vector3 direction = Vector3.zero;
 
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
             direction += Vector3.forward;
-        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
-            direction +=Vector3.back;
-        }else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction += Vector3.back;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
             direction += Vector3.right;
-        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
-            direction+=Vector3.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction += Vector3.left;
         }
 
-        if(direction == Vector3.zero){
+        if (direction == Vector3.zero)
+        {
+            if (animatorSet != null)
+            {
+                animatorSet.SetBool("Jump", false);
+                animatorSet.SetBool("Die", false);
+
+            }
             return;
         }
         Move(direction);
 
     }
 
-    public void Move(Vector3 direction){
+    public void Move(Vector3 direction)
+    {
         var targetPosition = transform.position + direction;
-        
+
         if (targetPosition.x < leftMoveLimit
             || targetPosition.x > rightMoveLimit
             || targetPosition.z < backMoveLimit
@@ -66,30 +87,37 @@ public class Duck : MonoBehaviour
             targetPosition = transform.position;
         }
 
+
+
+        //Animasi Lompat menggunakan DoTween
         transform.DOJump(
-            targetPosition,
-            jumpHeight,
-            1,
-            moveDuration
-        ).onComplete = BroadCastPositionOnJumpEnd;
-        
+                    targetPosition,
+                    jumpHeight,
+                    1,
+                    moveDuration
+                ).onComplete = BroadCastPositionOnJumpEnd;
+
+
         transform.forward = direction;
     }
 
-    public void SetNotMoveAble(bool value){
+    public void SetNotMoveAble(bool value)
+    {
         isNotMoveable = value;
     }
 
-    public void UpdateMoveLimit(int horizontalSize, int backLimit){
-        leftMoveLimit = -horizontalSize/2;
-        rightMoveLimit = horizontalSize/2;
+    public void UpdateMoveLimit(int horizontalSize, int backLimit)
+    {
+        leftMoveLimit = -horizontalSize / 2;
+        rightMoveLimit = horizontalSize / 2;
         backMoveLimit = backLimit;
     }
-    private void BroadCastPositionOnJumpEnd(){
+    private void BroadCastPositionOnJumpEnd()
+    {
         OnJumpEnd.Invoke(transform.position);
     }
 
-    //Ketika bertabrakan dengan collider maka mati
+    //Ketika bertabrakan dengan collider
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Car"))
@@ -98,11 +126,18 @@ public class Duck : MonoBehaviour
             {
                 return;
             }
-            transform.DOScale(new Vector3(1,0.1f,1), 0.2f);
+            if (animatorSet != null)
+            {
+                //animasi mati
+                animatorSet.SetBool("Die", true);
+            }else {
+                //Menjadikan character penyet
+                transform.DOScale(new Vector3(1, 0.1f, 1), 0.2f);
+            }
 
             isNotMoveable = true;
             OnCarCollision.Invoke();
-            Invoke("Die",3);
+            Invoke("Die", 3);
         }
         else if (other.CompareTag("Coin"))
         {
@@ -112,14 +147,16 @@ public class Duck : MonoBehaviour
         }
         else if (other.CompareTag("Eagle"))
         {
-            if(this.transform != other.transform){
+            if (this.transform != other.transform)
+            {
                 this.transform.SetParent(other.transform);
-                Invoke("Die",3);
+                Invoke("Die", 3);
 
             }
         }
     }
-    private void Die(){
+    private void Die()
+    {
         OnDie.Invoke();
     }
 
