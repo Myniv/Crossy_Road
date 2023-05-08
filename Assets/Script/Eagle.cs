e8ec15d75e132f826e439f1d532e822c37da0e55
mulyanan26 <mulyanan.ur26@gmail.com>
2023-05-08 16:18:29 +0700

    Update Eagle.cs

--- Assets/Script/Eagle.cs
+++ Assets/Script/Eagle.cs
@@ -5,26 +5,13 @@ using UnityEngine;
 public class Eagle : MonoBehaviour
 {
     [SerializeField, Range(0, 50)] float speed = 20;
-    bool pause=false;
 
 
     private void Update()
     {
-        if(pause==true){
-            EaglePause(true);
-        } else{
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
     }
 
-    public void EaglePauseSpeed(float speedEagle){
-        pause=true;
-        transform.Translate(Vector3.forward * speedEagle * Time.deltaTime);
-    }
-
-    public void EaglePause(bool pauseEagle){
-        pause=pauseEagle;
-        EaglePauseSpeed(0);
-    }
+    
 
 }
